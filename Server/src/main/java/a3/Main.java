ca862328084d7d52752d2875ba0e348f36681f9d
wvxbs <gabriel.ferreira7854@gmail.com>
2022-12-12 01:15:04 -0300

    new Client and Server projects added

--- Server/src/main/java/a3/Main.java
+++
@@ -1,7 +0,0 @@
-package a3;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ Server/src/main/java/a3/Server/ServerApplication.java
@@ -0,0 +1,13 @@
+package a3.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
